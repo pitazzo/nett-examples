c897fdce8b8eb4e040367392b109ef7cb0cda9eb
Pedro Malo <pedromalo@live.com>
2024-09-04 19:25:08 +0200

    remove: node_modules and dist

--- nestjs/tasks-api/dist/tasks/tasks.interface.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Task {
-    id: number;
-    name: string;
-    topic: 'compras' | 'casa' | 'trabajo' | 'familia';
-    isCompleted: boolean;
-    priority: 'LOW' | 'MEDIUM' | 'HIGH';
-}
